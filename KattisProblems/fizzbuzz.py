cdd0af76feb5ac89896e7be3ac7583a93497b78b
Akshdeep <akshdeep.s@live.com>
2017-07-01 18:57:04 -0400

    added new problems

---
+++ KattisProblems/fizzbuzz.py
@@ -0,0 +1,14 @@
+XYN = input().split(' ')
+x = int(XYN[0])
+y = int(XYN[1])
+N = int(XYN[2])
+
+for i in range(1, N + 1):
+    if i % x == 0 and i % y == 0:
+        print("FizzBuzz")
+    elif i % x == 0:
+        print("Fizz")
+    elif i % y ==0:
+        print("Buzz")
+    else:
+        print(i)
--- KattisProblems/pizzacrust.py
+++ KattisProblems/pizzacrust.py
@@ -1,8 +1,11 @@
 rAndC = input().split(' ')
+
 r = int(rAndC[0])
 c = int(rAndC[1])
 pi = 3.141592654
+
+
 if r == c:
     print('0.0')
 else:
-    print(((pi * c ** 2) / (pi * r ** 2)) * 100)
+    print(round(((pi * (r - c) ** 2) / (pi * r ** 2)) * 100, 8))
