b879a30fb5335e84e484f6066588859d2cf7c88d
Akshdeep <akshdeep.s@live.com>
2017-07-15 01:34:16 -0400

    added unfinished problems july 15

--- KattisProblems/beehives.py
+++ KattisProblems/beehives.py
@@ -1,12 +1,10 @@
 while True:
     dN = input().split(' ')
-
-    sour = 0
-    sweet = 0
     xy = []
     d = float(dN[0])
     N = int(dN[1])
-
+    magnitude = []
+    sour, sweet = 0, 0
     if dN == ['0.0', '0']:
         break
 
@@ -14,11 +12,16 @@ while True:
         xyCoordinates = input().split(' ')
         xy.append(xyCoordinates)
 
-    for j in range(len(xy)-1):
-        magnitude = ((float(xy[j + 1][0]) - float(xy[j][0])) ** 2 + (float(xy[j + 1][1]) - float(xy[j][1])) ** 2) ** 0.5
-        if magnitude <= d:
+    xy = sorted(xy)
+
+    for j in range(len(xy) - 1):
+        magnitude.append((float(xy[j][0]) - float(xy[j + 1][0])) ** 2 + ((float(xy[j][1]) - float(xy[j + 1][1])) ** 2) ** 0.5)
+
+    for k in magnitude:
+        print(k)
+        if k <= d:
             sour += 1
         else:
             sweet += 1
 
-    print(str(sour + 1), "sour,", str(sweet), "sweet")
+    print(str(sour), 'sour', str(sweet), 'sweet')
