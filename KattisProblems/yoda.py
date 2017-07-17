1ed30e117da58e74ef3263eaae4f51b594638d4d
Akshdeep Sharma <akshdeep.s@live.com>
2017-07-17 17:51:48 -0400

    semifixed yoda

--- KattisProblems/yoda.py
+++ KattisProblems/yoda.py
@@ -1,64 +1,49 @@
-num1 = list(input())
-num2 = list(input())
-
-num1 = num1[::-1]
-num2 = num2[::-1]
-
-if len(num1) > len(num2):
-    num3 = num2
-    num4 = num1
-else:
-    num3 = num1
-    num4 = num2
-
-for i in range(len(num3)):
-    if num3[i] > num4[i]:
-        num4[i] = '-'
-    elif num3[i] < num4[i]:
-        num3[i] = '-'
-
-
-def clean(num):
-    count = num.count('-')
-    for j in range(count):
-        num.remove('-')
-
-
-def zero(num):
-    for p in range(len(num)):
-        if num[p] == 0 and len(num) > 0:
-            del num[i]
+num1 = input()[::-1]
+num2 = input()[::-1]
+answer1 = []
+answer2 = []
+count1 = len(num1)
+count2 = len(num2)
+if len(num1) >= len(num2):
+    for i in range(len(num2)):
+        if num2[i] > num1[i]:
+            answer2.insert(0, num2[i])
+        elif num2[i] < num1[i]:
+            answer1.insert(0, num1[i])
         else:
-            break
-
-clean(num3)
-clean(num4)
-num3 = num3[::-1]
-num4 = num4[::-1]
-num3 = ''.join(num3)
-num4 = ''.join(num4)
-zero(num3)
-zero(num4)
-
+            answer2.insert(0, num2[i])
+            answer1.insert(0, num1[i])
+elif len(num1) < len(num2):
+    for i in range(len(num1)):
+        if num1[i] > num2[i]:
+            answer2.insert(0, num1[i])
+        elif num1[i] < num2[i]:
+            answer1.insert(0, num2[i])
+        else:
+            answer2.insert(0, num1[i])
+            answer1.insert(0, num2[i])
 if len(num1) > len(num2):
-    print(num4)
-    if len(num3) == 0:
-        print('YODA')
-    else:
-        print(num3)
-elif len(num2) < len(num1):
-    if len(num3) == 0:
-        print('YODA')
-    else:
-        print(num3)
-    print(num4)
-else:
-    if len(num3) == 0:
-        print('YODA')
-    else:
-        print(num3)
-    if len(num4) == 0:
-        print('YODA')
-    else:
-        print(num4)
+    for i in range(count2, count1):
+        answer1.insert(0, num1[i])
+elif len(num1) < len(num2):
+    for i in range(count1, count2):
+        answer1.insert(0, num2[i])
+answer1 = [int(x) for x in answer1]
+answer2 = [int(x) for x in answer2]
 
+if len(answer1) == 0:
+    answer1.append('YODA')
+elif sum(answer1) == 0:
+    answer1 = ['0']
+if len(answer2) == 0:
+    answer2.append('YODA')
+elif sum(answer2) == 0:
+    answer2 = ['0']
+answer1 = [str(x) for x in answer1]
+answer2 = [str(x) for x in answer2]
+print(''.join(answer1))
+print(''.join(answer2))
+
+'''
+    all logic = good except the numbers dont print out in right order sometimes (if num2 len > num1
+'''
