f3074166fe085afde11fbea0c3caabf9d31bfa28
Akshdeep <akshdeep.s@live.com>
2017-07-27 22:18:36 -0400

    Updated trees

--- DataStructuresLearning/trees.py
+++ DataStructuresLearning/trees.py
@@ -109,3 +109,16 @@ r.getRightChild().setRootVal('hello')
 print(r.getRightChild().getRootVal())
 
 # On 6.6. Review all of 6 cause you were dead STILL
+
+'''
+Create an empty tree.
+Read ( as the first token. By rule 1, create a new node as the left child of the root. Make the current node this new child.
+Read 3 as the next token. By rule 3, set the root value of the current node to 3 and go back up the tree to the parent.
+Read + as the next token. By rule 2, set the root value of the current node to + and add a new node as the right child. The new right child becomes the current node.
+Read a ( as the next token. By rule 1, create a new node as the left child of the current node. The new left child becomes the current node.
+Read a 4 as the next token. By rule 3, set the value of the current node to 4. Make the parent of 4 the current node.
+Read * as the next token. By rule 2, set the root value of the current node to * and create a new right child. The new right child becomes the current node.
+Read 5 as the next token. By rule 3, set the root value of the current node to 5. Make the parent of 5 the current node.
+Read ) as the next token. By rule 4 we make the parent of * the current node.
+Read ) as the next token. By rule 4 we make the parent of + the current node. At this point there is no parent for + so we are done.
+'''
