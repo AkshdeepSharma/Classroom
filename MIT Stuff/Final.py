69e4a9fe2f0b9901e5abf2fbbffcc55eefacdc83
Akshdeep <akshdeep.s@live.com>
2017-08-01 03:59:07 -0400

    added MIT final exam

--- MIT Stuff/Final.py
+++ MIT Stuff/Final.py
@@ -1,17 +1,25 @@
 '''
+PROBLEM 1:
+
 1-1: False
 1-2: True
 1-3: True
 1-4: True
 1-5: False
+'''
+
+'''
+PROBLEM 2:
 
 2-1: When a = A()
 2-2: O(n)
 2-3: False because a dictionary is mutable
 2-4: Worst case foo_two is worse than foo_one
 2-5: Polynomial
+'''
 
--------------------------
+'''
+PROBLEM 3:
 
 
 def sum_digits(s):
@@ -24,7 +32,10 @@ def sum_digits(s):
         return ValueError
     else:
         return sum(sums)
--------------------------
+'''
+
+'''
+PROBLEM 4:
 
 import collections
 
@@ -37,8 +48,10 @@ def max_val(t):
             return [t]
     flat = flatten(t)
     return max(flat)
--------------------------
+'''
 
+'''
+PROBLEM 5:
 
 def cipher(map_from, map_to, code):
     key_code = {}
@@ -48,6 +61,77 @@ def cipher(map_from, map_to, code):
     for i in code:
         decoded += key_code.get(i)
     return key_code, decoded
+'''
+
+'''
+PROBLEM 6:
+
+class Container(object):
+    def __init__(self):
+        self.vals = {}
+
+    def insert(self, e):
+        try:
+            self.vals[e] += 1
+        except:
+            self.vals[e] = 1
+
+    def __str__(self):
+        s = ""
+        for i in sorted(self.vals.keys()):
+            if self.vals[i] != 0:
+                s += str(i)+":"+str(self.vals[i])+"\n"
+        return s
+
+
+class Bag(Container):
+    def remove(self, e):
+        try:
+            self.vals[e] -= 1
+        except:
+            self.vals[e] = 0
+
+    def count(self, e):
+        try:
+            return self.vals[e]
+        except:
+            return 0
+
+    def __add__(self, other):
+        output = ''
+        outputList = []
+
+        for i in sorted(self.vals.keys()):
+            if i in other.vals.keys():
+                outputList.append([i, self.vals[i] + other.vals[i]])
+            else:
+                outputList.append([i, self.vals[i]])
+        for j in sorted(other.vals.keys()):
+            if j not in self.vals.keys():
+                outputList.append([j, other.vals[j]])
+
+        outputList = sorted(outputList)
+        for k in outputList:
+            output += str(k[0]) + ":" + str(k[1]) + "\n"
+        return output
+
+
+class ASet(Container):
+    def remove(self, e):
+        try:
+            del self.vals[e]
+        except:
+            pass
+
+    def is_in(self, e):
+        if e in self.vals:
+            return True
+        else:
+            return False
+'''
+
+'''
+PROBLEM 7:
 
 '''
 
