48a651ed9bf64eed499df3d00f810301fb6c0e40
Akshdeep <akshdeep.s@live.com>
2017-08-06 04:29:26 -0400

    added new problems

--- KattisProblems/falsesecurity.py
+++ KattisProblems/falsesecurity.py
@@ -7,6 +7,44 @@ translate = {
     'F': '..-.',
     'G': '--.',
     'H': '....',
-    'I': '..'
-    'J'
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    '_': '..--',
+    ',': '.-.-',
+    '.': '---.',
+    '?': '----'
 }
+
+try:
+    while True:
+        code = []
+        message = input()
+        translatedMessage = ''
+        decodedMessage = ''
+        for i in message:
+            translatedMessage += translate.get(i), ' '
+            code.append(len(translate.get(i)))
+        code = code[::-1]
+        for i in range(len(code) - 1):
+            decodedMessage += translatedMessage[list(translate.keys())[list(translate.values()).index(code[i])]]
+        print(decodedMessage)
+except EOFError:
+    pass
+
+
