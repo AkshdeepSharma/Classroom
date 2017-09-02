1a8e04975479cc2c8ada4b2a5787b774cc81e563
Akshdeep <akshdeep.s@live.com>
2017-09-02 02:57:22 -0400

    updated reddit bot project

--- Reddit Bot/reddit_bot.py
+++ Reddit Bot/reddit_bot.py
@@ -1,24 +1,17 @@
 import praw
-import time
 import pyperclip
+
 
 # allows the bot to log into reddit servers. Be sure to change "WorldDudeMan" to whatever your praw.ini contains as the
 # header. user_agent can be anything
-
-
 def bot_login():
-    reddit = praw.Reddit('WorldDudeMan', user_agent = "world news puller but I love cats")
-    print ('Logged in.')
+    reddit = praw.Reddit('SubredditManager', user_agent="Subreddit Manager")
     return reddit
-
-# copy and paste to the .txt file and to the private subreddit. limit can be changed from 15 (line 16), .txt file name
-# can be changed (line 20), subreddit name must be changed to your private subreddit (line 25). If you don't want to
-# post to a .txt file, remove lines 20-24.
 
 
 def paste_from_subreddit(subreddit_name, filter_word=''):
     for submission in reddit.subreddit(subreddit_name).hot(limit=15):
-        if (filter_word) in submission.title:
+        if filter_word in submission.title:
             threadURL = submission.url
             threadTitle = submission.title
             file = open("subredditlinks.txt", 'a')
@@ -27,21 +20,17 @@ def paste_from_subreddit(subreddit_name, filter_word=''):
             pyperclip.copy(threadURL + '\n')
             file.write(pyperclip.paste())
             reddit.subreddit('TestYourBeepBoop').submit(threadTitle, url=threadURL)
-             
-# initialize bot, run the paste command for each subreddit you want to post from. Note that image links will post the
-# URL as the image hosting site, self posts (i.e. AskReddit) will link to the post, and external links (i.e. news
-# outlets) will link to the website. The bot will then sleep for 12 hours before posting again. Below are some examples
-# of subreddits to access.
+
+
+# initialize bot, run the paste command for each subreddit you want to post from
+def main():
+    paste_from_subreddit('worldnews')
+    paste_from_subreddit('programming')
+    paste_from_subreddit('askreddit')
+    paste_from_subreddit('technology')
+    paste_from_subreddit('explainlikeimfive')
+    paste_from_subreddit('cscareerquestions')
+    paste_from_subreddit('learnprogramming')
 
 reddit = bot_login()
-while True:
-    paste_from_subreddit('worldnews', 'Trump')
-    paste_from_subreddit('mechanicalkeyboards', 'board')
-    paste_from_subreddit('personalfinance', 'debt')
-    paste_from_subreddit('askreddit')
-    paste_from_subreddit('aww')
-    paste_from_subreddit('explainlikeimfive')
-    paste_from_subreddit('showerthoughts')
-    paste_from_subreddit('learnprogramming')
-    print("Sleeping.")
-    time.sleep(43200)
+main()
