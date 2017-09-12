9231258b7d42c18085689e1d919cf4c3181e18d3
Akshdeep Sharma <akshdeep.s@live.com>
2017-09-12 06:44:16 -0400

    added scikitlearn stuff

---
+++ Scikit Learn/dogexample.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+greyhounds = 500
+labs = 500
+
+grey_height = 28 + 4 * np.random.randn(greyhounds)
+lab_height = 24 + 4 * np.random.randn(labs)
+
+plt.hist([grey_height, lab_height], stacked=True, color=['r', 'b'])
+plt.show()
