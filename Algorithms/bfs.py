c64244ea954323309638666663ed8d2fcf60afce
Akshdeep <akshdeep.sharma1@gmail.com>
2019-01-22 17:51:46 -0500

    redid bfs and dfs algos

--- Algorithms/bfs.py
+++ Algorithms/bfs.py
@@ -1,23 +1,17 @@
-vertexList = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-edgeList = [(0, 1), (1, 2), (1, 3), (3, 4), (4, 5), (1, 6)]
-graphs = (vertexList, edgeList)
-
-
 def bfs(graph, start):
-    vertexList, edgeList = graph
-    visitedList = []
-    queue = [start]
-    adjacencyList = [[] for vertex in vertexList]
-
-    for edge in edgeList:
-        adjacencyList[edge[0]].append(edge[1])
-
+    visited, queue = set(), [start]
     while queue:
-        current = queue.pop()
-        for neighbor in adjacencyList[current]:
-            if not neighbor in visitedList:
-                queue.insert(0,neighbor)
-        visitedList.append(current)
-    return visitedList
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
 
-print(bfs(graphs, 0))
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+print(bfs(graph, 'A'))
