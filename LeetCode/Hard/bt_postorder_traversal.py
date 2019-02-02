651255d8744638c8925abc8e430580abd1a372f3
Akshdeep <akshdeep.sharma1@gmail.com>
2019-02-02 03:25:23 -0500

    Added new problemos

---
+++ LeetCode/Hard/bt_postorder_traversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        self.helper(root, res)
+        return res
+
+    def helper(self, root, res):
+        if root:
+            self.helper(root.left, res)
+            self.helper(root.right, res)
+            res.append(root.val)
