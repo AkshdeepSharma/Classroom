a3c6d28c692400d5807380f517fdb2b3fe46b833
Akshdeep <akshdeep.sharma1@gmail.com>
2019-05-12 17:21:21 -0400

    updated code

--- LeetCode/Medium/814_binary_tree_pruning.py
+++ LeetCode/Medium/814_binary_tree_pruning.py
@@ -9,10 +9,8 @@ class Solution:
     def pruneTree(self, root: TreeNode) -> TreeNode:
         if root is None:
             return
-        if root.left:
-            root.left = self.pruneTree(root.left)
-        if root.right:
-            root.right = self.pruneTree(root.right)
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
         if not root.left and not root.right and root.val == 0:
             return None
         return root
