6f3fc2b68935cd977744038ca6c47d3a6a6782d8
Akshdeep <akshdeep.sharma1@gmail.com>
2019-05-11 20:25:08 -0400

    Added medium problem 450

---
+++ LeetCode/Medium/450_delete_node_in_BST.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            else:
+                temp = root.left
+                while temp.right:
+                    temp = temp.right
+                root.val = temp.val
+
+                root.left = self.deleteNode(root.left, temp.val)
+        return root
