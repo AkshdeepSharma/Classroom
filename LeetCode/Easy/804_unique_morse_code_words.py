58907833909ec7244a9a5343cbfbcc0d79abae4f
Akshdeep <akshdeep.sharma1@gmail.com>
2019-05-14 15:42:12 -0400

    Added easy problem 804

---
+++ LeetCode/Easy/804_unique_morse_code_words.py
@@ -0,0 +1,36 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse = {
+            'a': ".-",
+            'b': "-...",
+            'c': "-.-.",
+            'd': "-..",
+            'e': ".",
+            'f': "..-.",
+            'g': "--.",
+            'h': "....",
+            'i': "..",
+            'j': ".---",
+            'k': "-.-",
+            'l': ".-..",
+            'm': "--",
+            'n': "-.",
+            'o': "---",
+            'p': ".--.",
+            'q': "--.-",
+            'r': ".-.",
+            's': "...",
+            't': "-",
+            'u': "..-",
+            'v': "...-",
+            'w': ".--",
+            'x': "-..-",
+            'y': "-.--",
+            'z': "--.."
+        }
+        for i in range(len(words)):
+            temp = ""
+            for c in words[i]:
+                temp += morse[c]
+            words[i] = temp
+        return len(set(words))
