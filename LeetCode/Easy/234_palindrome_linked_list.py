6d1b014640d2f6295f1c8d93a4fbd04831c80367
Akshdeep <akshdeep.sharma1@gmail.com>
2019-05-18 23:55:31 -0400

    Added easy problems 125 and 234

---
+++ LeetCode/Easy/234_palindrome_linked_list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        slow = fast = head
+        rev = None
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+        while slow:
+            if slow.val != rev.val:
+                return False
+            slow, rev = slow.next, rev.next
+        return True
