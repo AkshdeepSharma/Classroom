53cdb6fa439e5c9c024e3e8ee45c1bd79d3ac919
Akshdeep <akshdeep.sharma1@gmail.com>
2019-05-26 05:27:36 -0400

    Added easy problem 151

---
+++ LeetCode/Easy/151_height_checker.py
@@ -0,0 +1,8 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        heights_sorted = sorted(heights)
+        count = 0
+        for i in range(len(heights)):
+            if heights[i] != heights_sorted[i]:
+                count += 1
+        return count
