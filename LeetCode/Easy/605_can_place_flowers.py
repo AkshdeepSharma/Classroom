bf55371376cd70415702b295ae881e07db3d4e15
Akshdeep <akshdeep.sharma1@gmail.com>
2019-06-08 01:25:46 -0400

    Added easy problem 605

---
+++ LeetCode/Easy/605_can_place_flowers.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i - 1] == flowerbed[i] == flowerbed[i + 1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+        return n <= 0
