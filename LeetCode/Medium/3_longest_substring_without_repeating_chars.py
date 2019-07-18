3496c15c95fa02d84dda4050cff67994768a694f
Akshdeep <akshdeep.sharma1@gmail.com>
2019-07-18 02:59:11 -0400

    Optimized medium problem 3

--- LeetCode/Medium/3_longest_substring_without_repeating_chars.py
+++ LeetCode/Medium/3_longest_substring_without_repeating_chars.py
@@ -1,16 +1,15 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        longest_substring = 0
-        s = list(s)
-        print(s)
-        dict = {}
-        i = j = 0
-        while j < len(s):
-            if s[j] in dict:
-                longest_substring = max(longest_substring, len(dict))
-                dict = {}
-                i += 1
-                j = i
-            dict[s[j]] = 1
-            j += 1
-        return max(longest_substring, len(dict))
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {}
+        start = 0
+        longest_substring_length = 0
+        for i, char in enumerate(s):
+            if char in dic and start < dic[char] + 1:
+                start = dic[char] + 1
+            dic[char] = i
+            longest_substring_length = max(longest_substring_length, i - start + 1)
+        return longest_substring_length
