b0c7e4b3896e9e7f59ccce6aa360e5bca92f2dcf
Akshdeep <akshdeep.sharma1@gmail.com>
2019-07-24 20:55:39 -0400

    Optimized code for easy problem 53

--- LeetCode/Easy/53_maximum_subarray.py
+++ LeetCode/Easy/53_maximum_subarray.py
@@ -1,11 +1,7 @@
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        dp = [0] * len(nums)
-        dp[0] = max_sum = nums[0]
+    def maxSubArray(self, nums):
+        max_sum = nums[0]
         for i in range(1, len(nums)):
-            if dp[i - 1] > 0:
-                dp[i] = nums[i] + dp[i - 1]
-            else:
-                dp[i] = nums[i]
-            max_sum = max(max_sum, dp[i])
+            nums[i] = max(nums[i], nums[i] + nums[i - 1])
+            max_sum = max(max_sum, nums[i])
         return max_sum
