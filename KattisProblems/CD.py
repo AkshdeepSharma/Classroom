11f0e54f981ccadb801b749e7d982668190f7e3a
Akshdeep <akshdeep.sharma1@gmail.com>
2019-10-06 20:23:33 -0400

    Updated CD.py

--- KattisProblems/CD.py
+++ KattisProblems/CD.py
@@ -1,14 +1,18 @@
-NM = input().split(' ')
-N, M = int(NM[0]), int(NM[1])
-CD = {}
-count = 0
-for i in range(N):
-    jill = int(input())
-    CD.update({jill: 1})
+def countCD(cd1, cd2):
+    hashset = set()
+    sell = 0
+    for _ in range(cd1):
+        catalog_num = input()
+        hashset.add(catalog_num)
+    for _ in range(cd2):
+        catalog_num = input()
+        if catalog_num in hashset:
+            sell += 1
+    return sell
 
-for j in range(M):
-    jack = int(input())
-    if jack in CD:
-        count += 1
-wtf = input()
-print(count)
+if __name__ == "__main__":
+    while True:
+        cd1, cd2 = map(int, input().split())
+        if cd1 == 0 and cd2 == 0:
+            break
+        print(countCD(cd1, cd2))
