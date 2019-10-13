333aff8823559265e1e975129d6ab90e3995b349
Akshdeep <akshdeep.sharma1@gmail.com>
2019-10-12 23:16:19 -0400

    Updated parsinghex.py (still doesn't work)

--- KattisProblems/parsinghex.py
+++ KattisProblems/parsinghex.py
@@ -1,31 +1,23 @@
 hex_numbers = []
-lines = []
-applicable = '1234567890abcdefABCDEFxX'
-# input
 try:
     while True:
         line = input()
-        if line:
-            lines.append(line)
-        else:
+        if line == "0":
             break
+        for i in range(len(line) - 2):
+            if line[i] == "0" and line[i + 1].lower() == "x":
+                append_index = i
+                i += 2
+                while True:
+                    try:
+                        if int(line[append_index: i + 1], 16):
+                            i += 1
+                            continue
+                    except (IndexError, ValueError):
+                        break
+                hex_numbers.append(line[append_index:i])
 except EOFError:
     pass
-lines = ''.join(lines)
 
-#  parsing
-for i in range(len(lines)):
-    temp = ''
-    k = i
-    if lines[i] == '0' and (lines[i + 1] == 'x' or lines[i + 1] == 'X'):
-        try:
-            while lines[k] in applicable and len(temp) <= 10:
-                temp += lines[k]
-                k += 1
-        except IndexError:
-            pass
-        hex_numbers.append(temp)
-
-# hex to dec
 for i in range(len(hex_numbers)):
     print(hex_numbers[i], int(hex_numbers[i], 16))
