1410e25f50dec5c1965d30868b3acf3c61f292bf
Akshdeep <akshdeep.sharma1@gmail.com>
2019-11-01 17:29:44 -0400

    Updated LC 1079

--- LeetCode/Medium/1079_letter_tile_possibilities.py
+++ LeetCode/Medium/1079_letter_tile_possibilities.py
@@ -1,3 +1,6 @@
+from itertools import permutations
+
+
 class Solution(object):
     def numTilePossibilities(self, tiles):
         """
@@ -5,6 +8,7 @@ class Solution(object):
         :rtype: int
         """
         hashset = set()
+
         def dfs(permutation, tiles):
             if permutation in hashset:
                 return
@@ -14,3 +18,9 @@ class Solution(object):
                 dfs(permutation + tiles[i], tiles[:i] + tiles[i + 1:])
         dfs('', tiles)
         return len(hashset) - 1
+
+    def numTilePossibilitiesUsingPermutation(self, tiles):
+        l = []
+        for j in range(1, len(tiles) + 1):
+            l.extend(list(permutations(list(tiles), j)))
+        return len(set(l))
