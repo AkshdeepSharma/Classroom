51c2e898bd7dcabf79eef6ed5e65f8e9de050f6f
Akshdeep <akshdeep.sharma1@gmail.com>
2019-11-01 16:09:16 -0400

    Updated code

--- LeetCode/Medium/807_city_skyline.py
+++ LeetCode/Medium/807_city_skyline.py
@@ -4,16 +4,12 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        sum_increased = 0
-        rows_max = [0] * len(grid)
-        cols_max = [0] * len(grid[0])
-
+        height_increase = 0
+        row_maxes = list(map(max, grid))
+        column_maxes = list(map(max, zip(*grid)))
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                rows_max[i] = max(rows_max[i], grid[i][j])
-                cols_max[j] = max(cols_max[j], grid[i][j])
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                sum_increased += min(rows_max[i], cols_max[j]) - grid[i][j]
-        return sum_increased
+            for j in range(len(grid)):
+                curr_num = grid[i][j]
+                height_increase -= curr_num - \
+                    min(row_maxes[i], column_maxes[j])
+        return height_increase
