ac6900f9f42b12cd91186a96cc95d08470ec7937
Akshdeep <akshdeep.sharma1@gmail.com>
2019-11-03 17:23:27 -0500

    Updated problem 442

--- LeetCode/Medium/442_find_all_duplicates_in_array.py
+++ LeetCode/Medium/442_find_all_duplicates_in_array.py
@@ -1,13 +1,8 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        d = {}
         res = []
-        for c in nums:
-            if c in d:
-                d[c] += 1
-            else:
-                d[c] = 1
-        for key, val in d.items():
-            if val == 2:
-                res.append(key)
+        for num in nums:
+            if nums[abs(num) - 1] < 0:
+                res.append(abs(num))
+            nums[abs(num) - 1] *= -1
         return res
