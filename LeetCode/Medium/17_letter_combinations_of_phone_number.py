8653a49655d36d906ad9fa91a91f0bb484ad49f4
Akshdeep <akshdeep.sharma1@gmail.com>
2019-11-10 18:31:27 -0500

    Renamed some Easy problems answered during contests to better reflect their new LeetCode problem number. Added Medium problem 17.

---
+++ LeetCode/Medium/17_letter_combinations_of_phone_number.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if digits == '':
+            return []
+
+        key = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        res = ['']
+        for num in digits:
+            temp = []
+            for i in res:
+                for j in key[num]:
+                    temp.append(i + j)
+            res = temp
+        return res
