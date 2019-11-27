2747528085bc1114c26087dc7bb3ec772fc4927a
Akshdeep <akshdeep.sharma1@gmail.com>
2019-11-26 19:53:32 -0500

    Updated walrusweights.py

--- Kattis/walrusweights.py
+++ Kattis/walrusweights.py
@@ -1,9 +1,30 @@
-N = int(input())
-weights = []
-for i in range(N):
-    weights.append(int(int(input())))
+def determine_weight(weights):
+    target_weight = 1000
+    best_weight = 0
+    visited = set()
+    visited.add(0)
+    if min(weights) > target_weight:
+        return min(weights)
+    for weight in weights:
+        for value in list(visited):
+            curr = abs(1000 - (weight + value))
+            if curr == target_weight and best_weight < weight + value:
+                best_weight = weight + value
+            elif curr < target_weight:
+                best_weight = weight + value
+                target_weight = curr
+            if curr > target_weight and weight + value > 1000:
+                continue
+            visited.add(weight + value)
+    return best_weight
 
-if all(w > 1000 for w in weights):
-    print(min(weights))
-    quit()
 
+def main():
+    num_weights = int(input())
+    weights = [int(input()) for weight in range(num_weights)]
+    weights = sorted(weights)
+    print(determine_weight(weights))
+
+
+if __name__ == '__main__':
+    main()
