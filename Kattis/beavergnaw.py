4e47b43047b0869ad25ab6f5f9ca7ce33d7d111d
Akshdeep <akshdeep.sharma1@gmail.com>
2020-01-02 18:29:23 -0500

    Add beavergnaw.py

--- Kattis/beavergnaw.py
+++ Kattis/beavergnaw.py
@@ -1,5 +1,16 @@
-DV = []
-while DV != ['0', '0']:
-    DV = input().split(' ')
-    DV = [int(x) for x in DV]
+from math import pi
 
+
+def gnaw(D, V):
+    return round((D ** 3 - 6 * (V / pi)) ** (1.0 / 3), 9)
+
+
+def main():
+    D, V = map(int, input().split())
+    while D != 0:
+        print(gnaw(D, V))
+        D, V = map(int, input().split())
+
+
+if __name__ == "__main__":
+    main()
