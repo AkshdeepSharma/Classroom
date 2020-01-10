1d27c07dbd1064dd7ff0106c20132330beb5fdf8
Akshdeep <akshdeep.sharma1@gmail.com>
2020-01-09 19:50:14 -0500

    Add problem 64

---
+++ LeetCode/Python/Medium/64_min_path_sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if i - 1 >= 0 and j - 1 >= 0:
+                    dp[i][j] = min(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j])
+                elif i - 1 >= 0:
+                    dp[i][j] = dp[i - 1][j] + grid[i][j]
+                elif j - 1 >= 0:
+                    dp[i][j] = dp[i][j - 1] + grid[i][j]
+        return dp[-1][-1]
