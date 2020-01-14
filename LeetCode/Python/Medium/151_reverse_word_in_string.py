d976fa1ca2a8bd8192193fcdbf04df3a119c4a27
Akshdeep <akshdeep.sharma1@gmail.com>
2020-01-13 20:22:24 -0500

    Update code for 151

--- LeetCode/Python/Medium/151_reverse_word_in_string.py
+++ LeetCode/Python/Medium/151_reverse_word_in_string.py
@@ -4,7 +4,4 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        list_s = s.split(' ')
-        list_s = list(reversed(list_s))
-        s = " ".join(list_s)
-        return " ".join(s.split())
+        return " ".join(reversed(s.split()))
