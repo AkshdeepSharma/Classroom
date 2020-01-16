60b5801fa21ae33557da5861e3e8cbe55dd64c7c
Akshdeep <akshdeep.sharma1@gmail.com>
2020-01-15 20:22:11 -0500

    Update 448 to be O(n) time and O(1) space (compared to O(n) time and O(n) space previously

--- LeetCode/Python/Easy/448_find_all_numbers_disappeared_in_array.py
+++ LeetCode/Python/Easy/448_find_all_numbers_disappeared_in_array.py
@@ -1,11 +1,11 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        d = {}
         res = []
         for num in nums:
-            if num not in d:
-                d[num] = 1
-        for i in range(1, len(nums) + 1):
-            if i not in d:
-                res.append(i)
+            if nums[abs(num) - 1] < 0:
+                continue
+            nums[abs(num) - 1] *= -1
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                res.append(i + 1)
         return res
