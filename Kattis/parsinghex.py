9f643d985b3b48db9312bc33ab03da8ee980d3a4
Akshdeep <akshdeep.sharma1@gmail.com>
2020-03-11 16:24:31 -0400

    changes to parsinghex.py

--- Kattis/parsinghex.py
+++ Kattis/parsinghex.py
@@ -1,23 +1,28 @@
-hex_numbers = []
-try:
-    while True:
-        line = input()
-        if line == "0":
-            break
-        for i in range(len(line) - 2):
-            if line[i] == "0" and line[i + 1].lower() == "x":
-                append_index = i
-                i += 2
-                while True:
-                    try:
-                        if int(line[append_index: i + 1], 16):
-                            i += 1
-                            continue
-                    except (IndexError, ValueError):
-                        break
-                hex_numbers.append(line[append_index:i])
-except EOFError:
-    pass
+def findHexNumbers(line):
+    ans = []
+    valid_chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                   'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'}
+    start_indices = [i + 2 for i in range(len(line)) if line.startswith(
+        '0x', i) or line.startswith('0X', i)]
+    for index in start_indices:
+        for i in range(index, len(line)):
+            if line[i] not in valid_chars:
+                ans.append(line[index - 2: i])
+                break
+    for hexNum in ans:
+        yield (hexNum, str(int(hexNum[2:], 16)))
 
-for i in range(len(hex_numbers)):
-    print(hex_numbers[i], int(hex_numbers[i], 16))
+
+def main():
+    try:
+        while True:
+            line = input()
+            ans = list(findHexNumbers(line))
+            for pair in ans:
+                print(pair[0] + " " + pair[1])
+    except EOFError:
+        pass
+
+
+if __name__ == "__main__":
+    main()
