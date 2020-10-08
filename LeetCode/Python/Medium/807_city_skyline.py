753f120e0f09eafb115343b07b323cbe68332711
Akshdeep <akshdeep.sharma1@gmail.com>
2020-10-08 15:14:20 -0400

    Update 807

--- LeetCode/Python/Medium/807_city_skyline.py
+++ LeetCode/Python/Medium/807_city_skyline.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def maxIncreaseKeepingSkyline(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        height_increase = 0
-        row_maxes = list(map(max, grid))
-        column_maxes = list(map(max, zip(*grid)))
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        ans = 0
+        row_maxes = [max(row) for row in grid]
+        col_maxes = list(map(max, zip(*grid)))
         for i in range(len(grid)):
-            for j in range(len(grid)):
-                curr_num = grid[i][j]
-                height_increase -= curr_num - \
-                    min(row_maxes[i], column_maxes[j])
-        return height_increase
+            for j in range(len(grid[i])):
+                ans += min(row_maxes[i], col_maxes[j]) - grid[i][j]
+        return ans
