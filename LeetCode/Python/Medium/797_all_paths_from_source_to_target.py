b68d34c2375ea56887e23eaae98548235e2e6b10
Akshdeep <akshdeep.sharma1@gmail.com>
2020-10-09 16:45:07 -0400

    Add 797

--- LeetCode/Python/Medium/797_all_paths_from_source_to_target.py
+++ LeetCode/Python/Medium/797_all_paths_from_source_to_target.py
@@ -1,23 +1,12 @@
-class Solution(object):
-    def allPathsSourceTarget(self, graph_list):
-        """
-        :type graph_list: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        start, end = 0, len(graph_list) - 1
-        graph = {}
-        res = []
-        for i in range(len(graph_list)):
-            graph[i] = set()
-            for j in range(len(graph_list[i])):
-                graph[i].add(graph_list[i][j])
-
-        def dfs(graph, start, end, path=[]):
-            path = path + [start]
-            if start == end:
-                res.append(path)
-            for next in graph[start]:
-                if next not in path:
-                    dfs(graph, next, end, path)
-        dfs(graph, start, end)
-        return res
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        paths = [[0]]
+        ans = []
+        while paths:
+            path = paths.pop()
+            for node in graph[path[-1]]:
+                if node == len(graph) - 1:
+                    ans.append(path + [node])
+                else:
+                    paths.append(path + [node])
+        return ans
