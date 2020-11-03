d3d84c286ee4e8c5158e59e2daf0acb7674e0a49
Akshdeep <akshdeep.sharma1@gmail.com>
2020-11-03 13:44:33 -0500

    Add medium problem 1637

---
+++ LeetCode/Python/Medium/1637_widest_vertical_area.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        max_width = 0
+        points = sorted(points, key=lambda x: x[0])
+        for i in range(1, len(points)):
+            max_width = max(max_width, points[i][0] - points[i - 1][0])
+        return max_width
