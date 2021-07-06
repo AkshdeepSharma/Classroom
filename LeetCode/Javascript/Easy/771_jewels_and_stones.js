944e7b41af0ccec3b79d77295421147849f86709
Akshdeep Sharma <akshdeep.sharma1@gmail.com>
2021-07-06 19:54:53 -0400

    Add Javascript easy problem 771

---
+++ LeetCode/Javascript/Easy/771_jewels_and_stones.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+*/
+var numJewelsInStones = function(jewels, stones) {
+    let ans = 0;
+    const setJewels = new Set(jewels);
+    for (let i=0; i < stones.length; i++) {
+        if (setJewels.has(stones[i])){
+            ans++;
+        }
+    }
+    return ans;
+};
