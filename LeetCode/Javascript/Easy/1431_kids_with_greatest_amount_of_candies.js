1dab2f206f0f4ebbeec6d52cd018e04bbaac1a47
Akshdeep Sharma <akshdeep.sharma1@gmail.com>
2021-07-06 20:24:57 -0400

    Add 1431

---
+++ LeetCode/Javascript/Easy/1431_kids_with_greatest_amount_of_candies.js
@@ -0,0 +1,11 @@
+var kidsWithCandies = function(candies, extraCandies) {
+    const maxCandies = Math.max(...candies);
+    let ans = [];
+    for(let i=0; i < candies.length; i++){
+        if(candies[i] + extraCandies >= maxCandies)
+            ans.push(true);
+        else
+            ans.push(false);
+    };
+    return ans;
+};
